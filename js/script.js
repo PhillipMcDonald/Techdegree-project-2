3ee940bec27e8f17958a8413ae5b74cdb9712d75
PhillipMcDonald <81326101+PhillipMcDonald@users.noreply.github.com>
2021-06-01 16:57:28 -0400

    added searchbar feature,  added missing "alts" to pictures
    I added a search bar function that adds a search bar to the page, and filters data based on the input. It works on "keyup" so the data is filtered in real time. I had to change the way I selected the first pagination button  to make this work.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+//data.forEach(elements=>console.log(elements.name['first'],elements.name['last']))
+
+
+
 
 
 /*
@@ -17,13 +21,17 @@ This function will create and insert/append the elements needed to display a "pa
 */
 
 function showPage (list, page){
+
    const startIndex = (page * 9)-9;
    const endIndex = (page*9);
    const ul = document.getElementsByClassName("student-list");
    const studentList = ul[0];
    studentList.innerHTML= "";
+
    for(let i =0; i<list.length; i++){
+
       if([i]>= startIndex && [i] < endIndex){
+
          const li = document.createElement('li');
          li.className = "student-item cf";
 
@@ -36,6 +44,7 @@ function showPage (list, page){
          const image = document.createElement('img');
          image.className = "avatar";
          image.src = list[i].picture["large"];
+         image.alt = "Profile Picture"
          firstDiv.appendChild(image);
 
          const h3= document.createElement('h3');
@@ -58,8 +67,8 @@ function showPage (list, page){
 
 
          studentList.appendChild(li)
-      
 
+      
 
       };
    };
@@ -76,24 +85,26 @@ This function will create and insert/append the elements needed for the paginati
 */
 
 function addPagination(list){
+
    const numbOfPages = Math.ceil(list.length/9)
    const ul = document.getElementsByClassName("link-list")
    const linkList = ul[0]
    linkList.innerHTML = ""
 
    for(let i = 1; i<=numbOfPages; i++){
+
       const secondLi = document.createElement('li')
       const btn = document.createElement('button')
+
       btn.type = "button"
       btn.textContent= [i]
 
       secondLi.appendChild(btn)
       linkList.appendChild(secondLi)
 
-      const firstButton = document.querySelector('button')
-      firstButton.className = "active"
-
-
+      let activeListItem = document.querySelector('.link-list').firstChild
+     let activeButton =  activeListItem.firstChild
+     activeButton.className ="active"
 
    }
 
@@ -105,13 +116,103 @@ function addPagination(list){
          showPage(list,e.target.textContent)
       }
    })
+
    return linkList
    }
+
+
    showPage(data,1)
    addPagination(data)
 
  
-
+function showNoResults(){
+   
+   let ul = document.querySelector('ul')
+   ul.textContent = "No Results"
+   
+}
 
 
 // Call functions
+
+function searchBar () {
+const  header = document.getElementsByClassName('header')
+      const searchBarLocation = header[0]
+
+      const searchLabel = document.createElement('label')
+      searchLabel.setAttribute('for', 'search')
+      searchLabel.className = "student-search"
+
+      const searchSpan = document.createElement('span')
+      searchSpan.textContent = "Search by Name"
+      searchLabel.appendChild(searchSpan)
+
+      const input = document.createElement('input')
+      input.id = "search"
+      input.placeholder = "Search by name..."
+      searchLabel.appendChild(input)
+
+      let searchButton = document.createElement('button')
+      searchButton.type = "button"
+      searchLabel.appendChild(searchButton)
+      
+      const searchImg = document.createElement('img')
+      searchImg.src = 'img/icn-search.svg'
+      searchImg.alt = "Search icon"
+      searchButton.appendChild(searchImg)
+      
+
+      searchBarLocation.appendChild(searchLabel)
+
+
+       
+
+searchLabel.addEventListener("keyup",(e)=>{
+   
+   const searchString = e.target.value.toLowerCase()
+   const splitString = searchString.trim().toString().toLowerCase().split(" ");
+ const filteredCharacters =  data.filter((student) =>{
+    for( let i = 0; i<=splitString.length; i++){
+    if( student.name['first'].toLowerCase().includes(splitString[i])||
+         student.name['last'].toLowerCase().includes(splitString[i])) { 
+
+      return (
+      student.name['first'].toLowerCase().includes(splitString[i])||
+      student.name['last'].toLowerCase().includes(splitString[i])
+      
+   
+      )
+      
+
+  } 
+  
+ 
+   }
+
+  }
+ 
+ )
+
+  
+ 
+  showPage(filteredCharacters,1)
+  addPagination(filteredCharacters)
+  if (filteredCharacters.length === 0){
+     showNoResults()
+  }
+
+  
+}
+
+
+
+
+
+)
+
+
+ 
+}
+
+searchBar()
+
